6540217cea428af64bc5401132dc8e45c0479bec
Het Shah <het.shah@vodafone.com>
2024-06-07 14:18:57 +0200

    fix(outputs): removing unwanted outputs

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,9 +1,7 @@
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
-}
-
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
-}
-
 output "public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = module.ec2.public_ip
