f6ba86deafc8240b2e3d654e434c565d2c6d75f0
Het Shah <het.shah@vodafone.com>
2024-06-18 11:12:00 +0200

    rename tfmoduleindex

--- scripts/tfmoduleindex.sh
+++ scripts/tfmoduleindex.sh
@@ -4,4 +4,4 @@ cwd=$(pwd)
 pushd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)" 1>/dev/null || exit
 pushd module_index 1>/dev/null || exit
 
-go run "./module_index.go" -dir="$cwd"
+go run "./tfmoduleindex.go" -dir="$cwd"
