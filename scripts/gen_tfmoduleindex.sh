17e4f6079fc10de89fe8cdb348cbaa687c497ce1
Het Shah <het.shah@vodafone.com>
2024-06-27 15:44:28 +0200

    fix(tfmoduleindex): rename

--- scripts/gen_tfmoduleindex.sh
+++ scripts/gen_tfmoduleindex.sh
@@ -2,6 +2,6 @@
 set -e -o pipefail
 cwd=$(pwd)
 pushd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)" 1>/dev/null || exit
-pushd tfmoduleindex 1>/dev/null || exit
+pushd gen_tfmoduleindex 1>/dev/null || exit
 
-go run "./tfmoduleindex.go" -dir="$cwd"
+go run "./gen_tfmoduleindex.go" -dir="$cwd"
